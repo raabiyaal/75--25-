08edb16aafc9f7ef5f4814f39546050be4b7121e
raabiyaal <rishaq@uchicago.edu>
2025-08-09 15:48:58 -0500

    update4

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ app.layout = html.Div([
 ])
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8051))
-    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+    port = int(os.environ.get("PORT", 8050))  # default 8050 if not set
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
 
 
